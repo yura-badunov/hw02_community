401ac169f1b5241ee2cee2d33e0279e0cab4e66c
Бадунов Юрий <dimaug@yandex.ru>
2022-05-06 19:33:36 +0300

    ревью 1

---
+++ yatube/posts/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    group = models.ForeignKey(
+        Group,
+        related_name='posts',
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
---
+++ yatube/posts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Group, Post
+
+
+def index(request):
+    title = 'Последние обновления на сайте'
+    posts = Post.objects.all()[:10]
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    title = f'Записи сообщества {group.title}'
+    posts = Post.objects.all()[:10]
+    context = {
+        'title': title,
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
